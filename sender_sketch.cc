46d60cc41921d02717ad4eb53b7f95990b5c08c3
KingWNTK <franciswyf@hotmail.com>
2020-03-03 10:48:34 -0600

    sketch update

--- sender_sketch.cc
+++ sender_sketch.cc
@@ -1,28 +1,71 @@
 
-typedef struct win_entry {
+struct win_entry {
     int seq_num;
     int retrans_cd;
-    char *s;
-    int file_offset;
-} win_entry;
+    int times_sent;
+    char *data;
+    int dsize;
+    ~win_entry() {
+        if (data) {
+            free(data);
+        }
+    }
+};
 
-typedef struct recv_packet {
+struct recv_packet {
     int ack;
     bool is_file_data;
-} recv_packet;
+};
 
-typedef struct send_packet {
+struct send_packet {
+};
 
-} send_packet;
+#define BATCH_SIZE 1024 * 512
+struct file_holder {
+    char data[BATCH_SIZE];
+    int offset_sent;
+    int buffer_size;
+    int tot_bytes_sent;
+    int tot_bytes;
+    int fd;
+};
+
+file_holder file;
+
 #define SEQ_MAX 128
 #define WIN_SIZE 64
 deque<win_entry> window;
 int window_sent;
 int window_size;
 
-int file_offset_max;
-
 int retrans_cd_max;
+
+int packet_dsize_max;
+
+char buf[BATCH_SIZE];
+
+bool refill_file_data() {
+    if (file.tot_bytes_sent == file.tot_bytes) {
+        return false;
+    }
+    file.buffer_size = min(BATCH_SIZE, file.tot_bytes - file.tot_bytes_sent);
+    //read min(BATCH_SIZE, tot_bytes - tot_bytes-snet) bytes starting from tot_bytes_sent from the fd
+    file.offset_sent = 0;
+    return true;
+}
+
+bool get_file_data(win_entry *info) {
+    if (file.offset_sent == file.buffer_size) {
+        if (!refill_file_data())
+            return false;
+    }
+    info->dsize = min(info->dsize, file.buffer_size - file.offset_sent);
+    info->data = malloc(info->dsize);
+    memcpy(info->data, file.data + file.offset_sent, info->dsize);
+    file.tot_bytes_sent += info->dsize;
+    file.offset_sent += info->dsize;
+    return true;
+}
 
 bool recv_data(char *buf) {
     if (!sanity_check(buf))
@@ -43,7 +86,7 @@ bool recv_data(char *buf) {
                 //if the filename and directory is received, init the window to send file data
                 window.clear();
                 for (int i = 1; i <= min(WIN_SIZE, file_offset_max); i++) {
-                    window.push_back(win_entry{(last_seq_num + i) % SEQ_MAX, 0, NULL, i});
+                    window.push_back(win_entry{(last_seq_num + i) % SEQ_MAX, 0, 0, NULL, packet_dsize_max});
                 }
             }
         } else {
@@ -53,7 +96,7 @@ bool recv_data(char *buf) {
             }
             for (int i = 0; i < sz; i++) {
                 if (window.back()->offset < file_offset_max) {
-                    window.push_back(win_entry{(last_seq_num + 1) % SEQ_MAX, 0, NULL, last->offset + 1});
+                    window.push_back(win_entry{(last_seq_num + 1) % SEQ_MAX, 0, 0, NULL, packet_dsize_max});
                 } else {
                     break;
                 }
@@ -61,20 +104,30 @@ bool recv_data(char *buf) {
         }
     }
 
-    if(window.empty()) return true;
+    if (window.empty())
+        return true;
     return false;
 }
 
-void send_data(win_entry *info) {
+bool send_data(win_entry *info) {
     /** 
-     * fill the buffer according to info
+     * fill the buffer
      */
-    sendTo(buf);
+    if (info->times_sent == 0) {
+        if (!get_file_data(info)) {
+            //the whole file is sent
+            return false;
+        }
+    }
+    memcpy(buf, info->data, info->dsize);
+    sendTo(buf, info->dsize);
     info->retrans_cd = retrans_cd_max;
+    info->times_sent++;
+    return true;
 }
 
 void update_retrans_cd() {
-    for(auto iter = window.begin(); iter != window.end(); iter++) {
+    for (auto iter = window.begin(); iter != window.end(); iter++) {
         iter->retrans_cd--;
     }
 }
@@ -104,8 +157,8 @@ void clock() {
 }
 
 int main() {
-    window.push_back({0, 0, "directory", 0});
-    window.push_back({1, 0, "filename", 0});
+    window.push_back({0, 0, 0, "directory", 0});
+    window.push_back({1, 0, 0, "filename", 0});
     while (1) {
         pthread_mutex_lock(&mutex);
         try_receive();
